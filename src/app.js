7b9527afdbe75f233d3f1d677dbe813c62ef1dce
Aliya Aftab <aftabaliya40@gmail.com>
2025-07-26 14:55:47 +0530

    routing examples

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express=require('express');
 const app=express();
+// app.get("/user",(req,res)=>{
+// console.log(req.query);
+// res.send(
+//     {firstname: "Aliya",
+//     lastname: "Aftab"}
+// )
+// })
 
-app.use("/test",(req, res)=>{
-res.send("Testing!")
+app.get("/user/:userId/:userPassword",(req,res)=>{
+console.log(req.params);
+res.send(
+    {firstname: "Aliya",
+    lastname: "Aftab"}
+)
 })
-// app.use((req, res)=>{
-// res.send("Hello from the server!")
-// })
-// app.use((req, res)=>{
-// res.send("Hello from the server!")
-// })
+
+app.use("/hello",(req, res)=>{
+res.send("Hello, Hello, Hello!")
+})
+
+app.use("/",(req, res)=>{
+res.send("Hello from the server!")
+})
+
 app.listen(3000, () => {
 console.log("Server is successfully listening on port 3000...");
 });
