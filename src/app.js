fc4f16e42cd1fd522c4facf8e65ad434a6ba5066
Aliya Aftab <aftabaliya40@gmail.com>
2025-08-01 14:24:29 +0530

    added express router

--- src/app.js
+++ src/app.js
@@ -1,159 +1,100 @@
 const express=require('express');
 const connectDB=require('./config/database')
 const app=express();
-const {validateSignUpData}=require('./utils/validation')
-const bcrypt=require('bcrypt');
-const User=require("./models/user");
+
 const cookieParser=require('cookie-parser')
 const jwt=require("jsonwebtoken");
-const {userAuth}=require('./middlewares/auth')
+
 app.use(express.json());
 app.use(cookieParser())
 // No authentication needed for signup and login api calls
-app.post("/signup",async (req, res)=>{
-try{
-// Validation of data
-validateSignUpData(req);
-// Encrypt the password
-const {firstName, lastName, emailId,password}=req.body;
-const passwordHash= await bcrypt.hash(password,10);
 
-// creating a new instance of the user
-const user=new User({
-firstName,
-lastName,
-emailId,
-password: passwordHash,
-});
-await user.save();
-res.send("User added successfully")
-}
-catch(err){
-res.status(400).send("Error: "+ err.message)
-}
-});
+const authRouter=require("./routes/auth")
+const profileRouter=require("./routes/profile")
+const requestRouter=require("./routes/request")
+// app.post("/signup",async (req, res)=>{
+// console.log(req.body)
+// const user=new User(
+// req.body
+// // firstName:"Rose",
+// // lastName:"Martin",
+// // emailId:"rossyy65@gmail.com",
+// // password:"0990@mori"
+// ); 
+// await user.save();
+// res.send("User added successfully")
+// })
 
-app.post("/login", async (req,res)=>{
-try{
-const {emailId, password}=req.body;
-const user= await User.findOne({emailId: emailId})
-if(!user){
-throw new Error("Invalid Credentials")
-}
-const isPasswordValid= await user.validatePassword(password)
-if(isPasswordValid){
-// create a jwt token
+// // get email by user
+// app.get("/user", async (req, res)=>{
+// const userEmail=req.body.emailId;
+// try{
+// const user=await User.findOne({emailId: userEmail});
+// if(user.length===0){
+// res.status(404).send("User not found")
+// }
+// else{
+// res.send(user)
+// }
+// }
+// catch(err){
+// res.status(400).send("Something went wrong")
+// }
+// })
 
-const token=await user.getJWT();
+// // Feed api 
+// app.get("/user", async (req, res)=>{
+// try{
+// const user=await User.find({});
+// res.send(user)
+// }
+// catch(err){
+// res.status(400).send("Something went wrong")
+// }
+// })
 
-// add the token to cookie and send back the response to user
-res.cookie("token", token,{httpOnly: true, expires: new Date(Date.now()+ 2*3600000) })
+// //  Delete api
+// app.delete("/user",async (req,res)=>{
+// const userId=req.body.userId;
+// try{
+// const user=await User.findByIdAndDelete(userId);
+// res.send("User deleted successfully")
+// }
+// catch(err){
+// res.status(400).send("Something went wrong");
+// }
+// })
+
+// // update data of the user
+// app.patch("/user/:userId", async(req,res)=>{
+// const userId=req.params?.userId;
+// const data=req.body;
+// try{
+// const ALLOWED_UPDATES=["photoUrl", "skill", "about"];
+// const isUpdateAllowed=Object.keys(data).every((k)=>
+// ALLOWED_UPDATES.includes(k)
+// );
+// if(!isUpdateAllowed){
+// throw new Error("Update not allowed");
+// }
+// if(data?.skills.length>10){
+// throw new Error("Skills cannot be more than 10")
+// }
+// const user=await User.findByIdAndUpdate({_id:userId},data,{
+// runValidators: true
+// });
+// console.log(user);
+// res.send("User updated successfully");
+// }
+// catch(err){
+// res.status(400).send("Something went wrong")
+// }
+// })
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 
-res.send("Login Successful")
-}
-else{
-throw new Error("Invalid Credentials")
-}
- 
-}
-catch(err){
- res.status(400).send(err.message || "Something went wrong");
-}
-
-})
-
-app.get("/profile", userAuth,async (req,res)=>{
-try{
-const user=req.user
-res.send(user)
-}
-catch(err){
-res.status(400).send(err.message || "Something went wrong")
-}
-})
-app.post("/sendConnectionRequest", userAuth,async(req, res)=>{
-const user=req.user;
-res.send(user.firstName +"send the Connection Request")
-})
-app.post("/signup",async (req, res)=>{
-console.log(req.body)
-const user=new User(
-req.body
-// firstName:"Rose",
-// lastName:"Martin",
-// emailId:"rossyy65@gmail.com",
-// password:"0990@mori"
-); 
-await user.save();
-res.send("User added successfully")
-})
-
-// get email by user
-app.get("/user", async (req, res)=>{
-const userEmail=req.body.emailId;
-try{
-const user=await User.findOne({emailId: userEmail});
-if(user.length===0){
-res.status(404).send("User not found")
-}
-else{
-res.send(user)
-}
-}
-catch(err){
-res.status(400).send("Something went wrong")
-}
-})
-
-// Feed api 
-app.get("/user", async (req, res)=>{
-try{
-const user=await User.find({});
-res.send(user)
-}
-catch(err){
-res.status(400).send("Something went wrong")
-}
-})
-
-//  Delete api
-app.delete("/user",async (req,res)=>{
-const userId=req.body.userId;
-try{
-const user=await User.findByIdAndDelete(userId);
-res.send("User deleted successfully")
-}
-catch(err){
-res.status(400).send("Something went wrong");
-}
-})
-
-// update data of the user
-app.patch("/user/:userId", async(req,res)=>{
-const userId=req.params?.userId;
-const data=req.body;
-try{
-const ALLOWED_UPDATES=["photoUrl", "skill", "about"];
-const isUpdateAllowed=Object.keys(data).every((k)=>
-ALLOWED_UPDATES.includes(k)
-);
-if(!isUpdateAllowed){
-throw new Error("Update not allowed");
-}
-if(data?.skills.length>10){
-throw new Error("Skills cannot be more than 10")
-}
-const user=await User.findByIdAndUpdate({_id:userId},data,{
-runValidators: true
-});
-console.log(user);
-res.send("User updated successfully");
-}
-catch(err){
-res.status(400).send("Something went wrong")
-}
-})
 
 connectDB()
 .then(()=>{
---
+++ src/routes/auth.js
@@ -0,0 +1,60 @@
+const express=require("express")
+const authRouter=express.Router();
+const {validateSignUpData}=require('../utils/validation')
+const User=require("../models/user");
+const bcrypt=require('bcrypt');
+
+authRouter.post("/signup",async (req, res)=>{
+try{
+// Validation of data
+validateSignUpData(req);
+// Encrypt the password
+const {firstName, lastName, emailId,password}=req.body;
+const passwordHash= await bcrypt.hash(password,10);
+
+// creating a new instance of the user
+const user=new User({
+firstName,
+lastName,
+emailId,
+password: passwordHash,
+});
+await user.save();
+res.send("User added successfully")
+}
+catch(err){
+res.status(400).send("Error: "+ err.message)
+}
+});
+
+authRouter.post("/login", async (req,res)=>{
+try{
+const {emailId, password}=req.body;
+const user= await User.findOne({emailId: emailId})
+if(!user){
+throw new Error("Invalid Credentials")
+}
+const isPasswordValid= await user.validatePassword(password)
+if(isPasswordValid){
+// create a jwt token
+
+const token=await user.getJWT();
+
+// add the token to cookie and send back the response to user
+res.cookie("token", token,{httpOnly: true, expires: new Date(Date.now()+ 2*3600000) })
+
+
+res.send("Login Successful")
+}
+else{
+throw new Error("Invalid Credentials")
+}
+ 
+}
+catch(err){
+ res.status(400).send(err.message || "Something went wrong");
+}
+
+})
+
+module.exports=authRouter
