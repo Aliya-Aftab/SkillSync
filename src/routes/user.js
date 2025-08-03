4271ab3202767772dff8260af4bda87e18d579c5
Aliya Aftab <aftabaliya40@gmail.com>
2025-08-03 19:32:53 +0530

    more api added

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const { userAuth } = require("../middlewares/auth");
 const mongoose=require("mongoose")
 const userRouter=express.Router();
 const ConnectionRequest=require("../models/connectionRequest")
-
+const User=require("../models/user")
 const USER_SAFE_DATA= ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills"];
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async(req, res)=>{
@@ -41,7 +41,39 @@ catch(err){
 res.status(400).send({message : err.message});
 }  
 })
+// user should see all the user card except:
+//his own card, his connections, ignored ones, already he had send the connection request
+userRouter.get("/feed", userAuth, async (req, res)=>{
+try{
+const loggedInUser=req.user;
+// find all connection request sent or received
+const connectionRequests= await ConnectionRequest.find({
+$or:[
+{fromUserId: loggedInUser},
+{toUserId: loggedInUser}
+]
+}).select("fromUserId toUserId");
+
+const hideUsersFromFeed=new Set();
+connectionRequests.forEach((req)=>{
+hideUsersFromFeed.add(req.fromUserId.toString());
+hideUsersFromFeed.add(req.toUserId.toString());
+});
+const users= await User.find({
+$and:[
+{_id : { $nin: Array.from(hideUsersFromFeed)}},
+{_id: {$ne: loggedInUser._id}},
+],
+}).select(USER_SAFE_DATA);
+res.status(200).json({                                   
+    message: "Users fetched successfully",
+    data: users
+  });
+}
+catch(err){
+res.status(400).json({message: err.message});
+}
+})
 
 
-
-module.exports=userRouter
+module.exports=userRouter;
